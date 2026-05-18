c948107a78601ff864fb47edc393182b47f95800
kubzal <jakub.zalewski@paramount.com>
2026-05-18 18:46:14 +0200

    flask - workshops (finished)

--- 34-flask-workshops/app.py
+++ 34-flask-workshops/app.py
@@ -6,6 +6,9 @@ CSV_FILE_PATH = "data/books.csv"
 
 app = Flask(__name__)
 
+history = []
+
+
 class Book:
     def __init__(self, title, author, book_count):
         self.title = title
@@ -30,9 +33,12 @@ def load_books():
 
         return books
 
+
 @app.route("/")
 def index():
+    history.append("index")
     return render_template("index.html", subtitle = "Home")
+
 
 @app.route("/add-book", methods=["GET", "POST"])
 def add_book():
@@ -64,12 +70,61 @@ def add_book():
             writer.writeheader()
             writer.writerows([book.to_dict() for book in books])
 
+        history.append("add book")
+
     return render_template("add_book.html", subtitle = "Add book")
+
+
+@app.route("/borrow-book", methods=["GET", "POST"])
+def borrow_book():
+    if request.method == "POST":
+        title = request.form.get("title")
+        author = request.form.get("author")
+        book_count = int(request.form.get("book_count"))
+
+        searched_book = Book(title, author, book_count)
+
+        print("Searched book:", searched_book)
+
+        with open(CSV_FILE_PATH, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+
+            books = []
+            for row in reader:
+                print(row)
+                if row["title"] == title and row["author"] == author:
+                    print("Book was found")
+                    available_books = int(row["book_count"])
+
+                    if available_books >= book_count:
+                        # enough books to borrow
+                        row["book_count"] = str(available_books - book_count)
+                    else:
+                        print(f"Not enough books of title '{row["title"]}' in the library")
+                books.append(row)
+
+        with open(CSV_FILE_PATH, "w", encoding="utf-8") as file:
+            fieldnames = ["title", "author", "book_count"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(books)
+
+        history.append("borrow book")
+
+    return render_template("borrow_book.html", subtitle = "Borrow book")
+
 
 @app.route("/books-list")
 def books_list():
     books = load_books()
+    history.append("books list")
     return render_template("books_list.html", subtitle="List of available books", books = books)
+
+
+@app.route("/operations-history")
+def operations_history():
+    return render_template("operations_history.html", subtitle="Operations history", history = history)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
